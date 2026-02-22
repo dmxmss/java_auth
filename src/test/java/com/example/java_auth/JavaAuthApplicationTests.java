a7b36be22a38ad502cec0672edc45d817e680349
dmxmss <dexammt@gmail.com>
2026-02-22 22:19:26 +0300

    initial commit

---
+++ src/test/java/com/example/java_auth/JavaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
