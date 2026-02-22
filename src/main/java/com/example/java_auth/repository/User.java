61819d89c54d57c6433bf62ea2f4a3b7a405c870
dmxmss <dexammt@gmail.com>
2026-02-22 22:32:21 +0300

    user entity & user repository

---
+++ src/main/java/com/example/java_auth/repository/User.java
@@ -0,0 +1,53 @@
+package com.example.java_auth.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    private String name;
+    private Integer score;
+    private String hashedPassword;
+
+    public User(Long id, String name, Integer score, String hashedPassword) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public User() {}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+}
